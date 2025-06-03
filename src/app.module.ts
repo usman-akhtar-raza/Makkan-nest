8406862cb27f38c5ab25a94742512c2bd6f3be81
usman <akhtarusman716@gmail.com>
2025-06-03 17:06:16 +0500

    change port to 3001 from 3000

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // allows access everywhere without re-importing
+    }),
+    MongooseModule.forRoot(process.env.MONGODB_URI ?? ''),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,8 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
+    const port = process.env.PORT;
+    console.log(port);
     return 'Hello World!';
   }
 }

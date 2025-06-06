2a24d832f764be18b828a221d53ac8bc1c1fe977
usman <akhtarusman716@gmail.com>
2025-06-07 02:57:00 +0500

    login dto added

---
+++ src/auth/dto/logindto/login.dto.ts
@@ -0,0 +1,14 @@
+import { IsEmail, IsString, Matches } from 'class-validator';
+import { Transform } from 'class-transformer';
+
+export class SignUpDto {
+  @IsEmail({}, { message: 'Please provide a valid email address' })
+  @Matches(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/, {
+    message: 'Email must be in a valid format',
+  })
+  @Transform(({ value }) => value.trim())
+  email: string;
+
+  @IsString()
+  password:string
+}
